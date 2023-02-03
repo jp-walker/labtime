cb6724ee8053e77a8e35caa9f1a9945c10ce5bee
Tyler Ting <juting@students.pitzer.edu>
2023-02-03 11:51:35 -0800

    Fixed Fixme.py

--- Fixme.py
+++ Fixme.py
@@ -5,6 +5,6 @@ def evens(n):
     '''
     Returns a list of even numbers from 0 to n inclusive.
     '''
-    evenslist = range(n+1)
+    evenslist = range(n + 1)
     evenslist = filter(lambda x: x % 2 == 0, evenslist)
     return list(evenslist)
